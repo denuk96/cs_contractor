91c5daeb698fd34a930a8e982db872702cc1baae
Denys Taradada <e_den9611@live.com>
2025-11-03 19:52:20 +0100

    Add `annotate` gem and configure schema annotations for models and factories

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ group :development, :test do
 end
 
 group :development do
+  gem 'annotate'
   gem "ffaker"
   gem "web-console"
   gem "webdrivers"
--- spec/factories/skin_items.rb
+++ spec/factories/skin_items.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: skin_items
+#
+#  id                 :integer          not null, primary key
+#  name               :string
+#  object_id          :string
+#  rarity             :integer
+#  wear               :integer
+#  collection_name    :string
+#  souvenir           :boolean
+#  stattrak           :boolean
+#  category           :string
+#  min_float          :float
+#  max_float          :float
+#  latest_steam_price :float
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 FactoryBot.define do
   factory :skin_item do
 
